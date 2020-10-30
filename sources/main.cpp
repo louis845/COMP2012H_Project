743add51fcbbfe6717fe9c8b6b9869b3edb4d90a
Louis Chau <louis321yh@gmail.com>
2020-10-31 00:59:16 +0800

    test

---
+++ sources/main.cpp
@@ -0,0 +1,16 @@
+/**
+ * Temporary main. Includes console for debugging.
+ * 
+*/
+
+#include <iostream>
+#include "math/test.h"
+
+using namespace std;
+
+int main(){
+    cout<<"Hello world!"<<endl;
+    long a=9;
+    long b=12;
+    cout<<gcd(a,b)<<endl;
+}
