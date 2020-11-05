d8f2f67810783e556c52f0ddee6082b627090ace
Louis Chau <louis321yh@gmail.com>
2020-11-05 20:11:30 +0800

    double update

--- sources/math/double/Double.h
+++ sources/math/double/Double.h
@@ -1,5 +1,8 @@
 #ifndef MATH_DOUBLE_DOUBLE_H
 #define MATH_DOUBLE_DOUBLE_H
+
+#include "math/Field.h"
+
 /**
  * Implementation of double, actually this just uses the primitive type.
 */
@@ -10,14 +13,14 @@ public:
 
     Double(double);
 
-    virtual Ring* addImpl (const Ring* r) const=0;
+    virtual Ring* addImpl (const Ring* r) const;
     
-    virtual Ring* minusImpl (const Ring* r) const=0;
+    virtual Ring* minusImpl (const Ring* r) const;
     
-    virtual Ring* multImpl (const Ring* r) const=0;
+    virtual Ring* multImpl (const Ring* r) const;
     
-    virtual Ring* divImpl (const Ring* div) const=0;
+    virtual Ring* divImpl (const Ring* div) const;
     
-    virtual Ring* remainderImpl (const Ring* div) const=0;
+    virtual Ring* remainderImpl (const Ring* div) const;
 };
 #endif
