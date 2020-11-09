c1c4e9d38e44242bfa95cacdff59f66c645ab383
Louis Chau <louis321yh@gmail.com>
2020-11-09 13:10:21 +0800

    Added implementation to complex integers

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <string>
 #include "math/long/Long.h"
+#include "math/long/LongComplex.h"
 #include "math/fraction/Fraction.h"
 #include "math/tools.h"
 
@@ -13,21 +14,20 @@ using namespace std;
 
 int main(){
     {
-        R a={new Long{-1920}};
-        R b={new Long{-1080}};
-        R c={new Fraction{a,b}};
-        R d={new Fraction{R{new Long{25}},new Long{7}}};
-
-        cout<<a<<"\n";
-        cout<<b<<"\n";
+        R c = {new Fraction{new LongComplex{-4584,54389},new LongComplex{342,54}}};
+        R d = {new Fraction{new LongComplex{342,54},new LongComplex{0,-1}}};
 
         cout<<c<<"\n";
-        cout<<d<<"\n";
+        cout<<d<<"\n\n";
         cout<<(c+d)<<"\n";
         cout<<(c*d)<<"\n";
+        cout<<(c/d)<<"\n";
+        cout<<(c%d)<<"\n\n";
 
-        cout<<a<<"\n";
-        cout<<b<<"\n";
+        cout<<(c/d)<<"\n";
+        cout<<(c%d)<<"\n\n";
+        
+        cout<<(d*(c/d)+(c%d))<<"\n";
     }
 
     int i;
