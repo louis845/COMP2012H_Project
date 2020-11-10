27db6a8a2792337ee5b316c0a922b6b1479257b4
zymk9 <zymk73@gmail.com>
2020-11-10 14:47:58 +0800

    Add the API for OCR

---
+++ sources/utils/test_ocr.cpp
@@ -0,0 +1,21 @@
+#include "ocr_api.h"
+#include <iostream>
+#include <string>
+#include <QCoreApplication>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    QCoreApplication app(argc, argv);
+    string filename;
+    cout << "Input the relative path for the image for testing." << endl;
+    cin >> filename;
+    string app_id = "";
+    string app_key = "";
+    Ocr::GetInstance().set_app_id(app_id);
+    Ocr::GetInstance().set_app_key(app_key);
+    auto res = Ocr::GetInstance().Request(filename);
+    cout << res.first << endl << res.second << endl;
+    cin.get(); cin.get();
+    return app.exec();
+}
