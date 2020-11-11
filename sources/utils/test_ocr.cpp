0171cdc5e943ef3ab6329c1dd031221752ad9bb0
zymk9 <zymk73@gmail.com>
2020-11-11 23:54:33 +0800

    Modify API and add token list

--- sources/utils/test_ocr.cpp
+++ sources/utils/test_ocr.cpp
@@ -10,8 +10,8 @@ int main(int argc, char* argv[])
     string filename;
     cout << "Input the relative path for the image for testing." << endl;
     cin >> filename;
-    string app_id = "";
-    string app_key = "";
+    string app_id = "";     // left blank
+    string app_key = "";    // left blank
     Ocr::GetInstance().set_app_id(app_id);
     Ocr::GetInstance().set_app_key(app_key);
     auto res = Ocr::GetInstance().Request(filename);
