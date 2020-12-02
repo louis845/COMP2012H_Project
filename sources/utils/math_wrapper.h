9625071c8213da0ebfe166ea4d0636290cbd5912
zymk9 <zymk73@gmail.com>
2020-12-02 20:07:14 +0800

    Update docs and fix minor bugs

--- sources/utils/math_wrapper.h
+++ sources/utils/math_wrapper.h
@@ -13,13 +13,17 @@
 #include <complex>
 #include <armadillo>
 
-// a generic wrapper for self-implemented math library
-// encapsulate some operations and add a class ROperand
-// to unify matrices and non-matrix expressions
+// a generic wrapper for self-implemented math library R
+// and the Armadillo library
+//
+// encapsulate some operations and unify matrices and 
+// scalars
 
-// note that the math library is not designed to be a high performance
+// NOTE: the R math library is not designed to be a high performance
 // scientific calculation/linear algebra library, hence optimization is very limited
 
+
+// Wrapper class for R scalar and matrix
 class ROperand 
 {
     friend class BinaryExprAst;
@@ -29,32 +33,51 @@ class ROperand
     friend class Info;
 
 public:
-    enum class Type { NOR, MAT, IMAT };             // IMAT : identity matrix 
+    // To distinguish the type of ROperand
+    // NOR: scalar  MAT: matrix  IMAT: identity matrix
+    enum class Type { NOR, MAT, IMAT };
 
     ROperand(): type(Type::IMAT) {}
+
+    // Instantiate from an R object
     explicit ROperand(R value): type(Type::NOR), value(std::move(value)) {}
+
+    // Instantiate an ROperand with type matrix, instantiate
+    // the vector mat with corresponding number of rows
     explicit ROperand(size_t row_num): type(Type::MAT), mat(row_num) {} 
+    
+    // Instantiate an ROperand with type matrix with a 2D array
+    // R**, along with its size
     explicit ROperand(R** matR, int row_num, int col_num);
     ~ROperand() = default;
 
     ROperand operator-();
-    ROperand operator+(ROperand rhs);           // use PBV and non-const since the implementation 
-    ROperand operator-(ROperand rhs);           // may change the values of the operands i.e. complexify
+    ROperand operator+(ROperand rhs);           // Use PBV and non-const parameter since the operation
+    ROperand operator-(ROperand rhs);           // may change the values of the operands e.g. complexify
     ROperand operator*(ROperand rhs);
     ROperand operator/(ROperand rhs);
     ROperand operator^(int rhs);
     ROperand operator+=(ROperand rhs);
     ROperand operator-=(ROperand rhs);
 
-    std::string genTex() const;         // generate TeX formated strings for rendering
+    // Generate LaTeX formated strings for rendering
+    std::string genTex() const;
 
 private:
     Type type;
     std::vector<std::vector<R>> mat;
-    R value;            // if type == NOR, value represents numerical value, otherwise the coefficient of IMAT
+
+    // If type == NOR, value represents a scalar value
+    // otherwise it represents the coefficient of the identity matrix
+    R value{new Long{1L}};
 };
 
 
+// Wrapper class for Armadillo, unify scalars, matrices and
+// identity matrices
+//
+// Scalars in all computation using Armadillo are assumed to be complex
+// numbers, and matrices are using complex entries by default 
 class ArmaOperand
 {
     friend class BinaryExprAst;
@@ -66,29 +89,49 @@ public:
     enum class Type { NOR, MAT, IMAT };
 
     ArmaOperand(): type(Type::IMAT) {}
+
+    // Instantiate a scalar from a complex number
     explicit ArmaOperand(const std::complex<double>& value): type(Type::NOR), value(value) {}
+
+    // Copy the matrix from an Armadillo built-in matrix type
     explicit ArmaOperand(arma::cx_mat mat): type(Type::MAT), mat(std::move(mat)) {}
+
+    // Instantiate a scalar by passing real and imaginary parts
     explicit ArmaOperand(double real, double imag=0.0): type(Type::NOR), value(real, imag) {}
+
+    // Instantiate a matrix by passing its size
+    //
+    // arma::fill::none means that the entries of the matrix will not be 
+    // initialized by value, but remain to be the original values in memory
     ArmaOperand(size_t row, size_t col): type(Type::MAT), mat(row, col, arma::fill::none) {}
-    ArmaOperand(Type type, const std::complex<double>& coeff): type(type), value(coeff) {}          // for IMAT
+
+    // Instantiate an identity matrix by passing its coefficient
+    ArmaOperand(Type type, const std::complex<double>& coeff): type(type), value(coeff) {}
 
     ArmaOperand operator+(const ArmaOperand& rhs) const;
     ArmaOperand operator-(const ArmaOperand& rhs) const;
     ArmaOperand operator*(const ArmaOperand& rhs) const;
     ArmaOperand operator/(const ArmaOperand& rhs) const;
     ArmaOperand operator^(const ArmaOperand& rhs) const;
-    arma::cx_mat fromImat(size_t row) const;                // return a square matrix with given size
 
+    // Return a square matrix, which is the product of the identity matrix
+    // and the coefficient
+    arma::cx_mat fromImat(size_t row) const;
+
+    // Test if the value is approximately a real number
     bool almostReal() const
     {
         return fabs(value.imag()) <= std::numeric_limits<double>::epsilon();
     }
 
+    // Generate LaTeX formatted string
     std::string genTex() const;
 
 private:
     Type type;
-    arma::cx_mat mat;              // entries by default are stored in double, cx = complex
+
+    // Entries by default are stored in double, cx = complex
+    arma::cx_mat mat;
     std::complex<double> value{1.0, 0.0};
 };
 
